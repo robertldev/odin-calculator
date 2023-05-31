f58f7842a899793e83ba24563ddbd1a3c6d70738
Rob <127467383+robertldev@users.noreply.github.com>
2023-05-31 16:15:35 +0100

    Finished calc layout and added click effects.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-var firstInput;
-var secondInput;
-var operatorInput;
+/* Add event listeners, and on-click effects */
 
-function calculateResult(firstNum, operator, secondNum) {
-    
-}
+let buttonNodeList = document.querySelectorAll('.button');
+
+buttonNodeList.forEach((button) => {
+    button.addEventListener('mousedown', () => {
+        button.style.boxShadow = 'inset 0px 3px #505050';
+    })
+    button.addEventListener('mouseup', () => {
+        button.style.boxShadow = '0px 3px #505050';
+    })
+})
