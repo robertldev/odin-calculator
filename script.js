db1e02101ead37820876c9f0a915cde8367b1df1
Rob <127467383+robertldev@users.noreply.github.com>
2023-06-05 16:34:30 +0100

    All func ready aside from equals button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let calcDisplay = document.querySelector('#display-content');
+let currentCalc = [];
 
 /* Add event listeners for on-click effects */
 let buttonNodeList = document.querySelectorAll('.button');
@@ -28,8 +29,68 @@ document.querySelector('#button-decimal').addEventListener('click', () => {
 
 /* Add functionality to the Clear button */
 document.querySelector('#button-clear').addEventListener('click', () => {
-    document.querySelector('#display-content').textContent = '0';
+    calcDisplay.textContent = '0';
+    currentCalc = [];
 })
+
+/* Add functionality to the positive/negative selector */
+document.querySelector('#button-posnev').addEventListener('click', () => {
+    if (!calcDisplay.textContent.includes('-')) {
+        calcDisplay.textContent = '-' + calcDisplay.textContent;
+    } else {
+        calcDisplay.textContent = calcDisplay.textContent.slice(1);
+    }
+})
+
+/* Function for updating the display when number or decimal is clicked */
+function updateDisplay(number) {
+    if (calcDisplay.textContent.length < 8 && calcDisplay.textContent != '0') {
+        calcDisplay.textContent = calcDisplay.textContent.concat(number);
+        /* This is to push new numbers on rather than add */
+        /* Also limiting the number of chars due to space, and also avoiding the possibility of a trail of 0's */
+    } else if (calcDisplay.textContent == 0) {
+        if (number == '.') {
+            calcDisplay.textContent = calcDisplay.textContent.concat(number);
+        } else {
+            calcDisplay.textContent = number;
+        }
+    }
+}
+
+function clearDisplay () {
+    calcDisplay.textContent = '';
+}
+
+/* Add event listener and push to calc array when operation is requested */
+document.querySelector('#button-add').addEventListener('click', () => {
+    let currentNum = parseFloat(calcDisplay.textContent);
+    if (!isNaN(currentNum)) {
+        currentCalc.push(currentNum, '+');
+        clearDisplay();
+    }
+})
+document.querySelector('#button-subtract').addEventListener('click', () => {
+    let currentNum = parseFloat(calcDisplay.textContent);
+    if (!isNaN(currentNum)) {
+        currentCalc.push(currentNum, '-');
+        clearDisplay();
+    } 
+})
+document.querySelector('#button-multiply').addEventListener('click', () => {
+    let currentNum = parseFloat(calcDisplay.textContent);
+    if (!isNaN(currentNum)) {
+        currentCalc.push(currentNum, '*');
+        clearDisplay();
+    }
+})
+document.querySelector('#button-divide').addEventListener('click', () => {
+    let currentNum = parseFloat(calcDisplay.textContent);
+    if (!isNaN(currentNum)) {
+        currentCalc.push(currentNum, '/');
+        clearDisplay();
+    }
+})
+
 
 /* Basic maths functions */
 addNumbers = (a, b) =>  a + b;
@@ -55,12 +116,26 @@ function operate(num1, num2, operator) {
     }
 }
 
-function updateDisplay(number) {
-    if (calcDisplay.textContent.length < 9 && calcDisplay.textContent != 0) {
-        calcDisplay.textContent = calcDisplay.textContent.concat(number);
-        /* This is to push new numbers on rather than add */
-        /* Also limiting the number of chars due to space, and also avoiding the possibility of a trail of 0's */
-    } else if (calcDisplay.textContent == 0) {
-        calcDisplay.textContent = number;
+/* Add event listener for equals and invoke calculation */
+document.querySelector('#button-equals').addEventListener('click', () => {
+    let currentNum = parseFloat(calcDisplay.textContent);
+    if (currentNum !== "") {
+        currentCalc.push(currentNum);
     }
+    if (currentCalc.length > 2) {
+        equalsCalc();
+    }
+})
+
+/* Now create our function to calculate from our array */
+function equalsCalc () {
+    if (isNaN(currentCalc[currentCalc.length - 1])) {
+        currentCalc.pop();
+    }
+    
 }
+
+/* Calculate and return a value */
+/* Check the output is max 9 numbers */
+/* Clear array at end */
+/* Decide how to start a new calculation */
